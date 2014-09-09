43049d8ea60482f70668b2f939e22eb246f83409
puentejavier <puentejavier80@gmail.com>
2014-09-09 21:16:58 +0200

    sumas y restas

---
+++ js/ejercicio.js
@@ -0,0 +1,19 @@
+
+//sumas sencillas
+
+var numUno = parseInt(prompt("introduzca codigo"));
+
+var numDos = parseInt(prompt("introduzca codigo"));
+
+var numTres = parseInt(prompt("introduzca texto"));
+
+var suma = numUno + numDos;
+
+var resultadoDef = (suma / numTres) % numTres;
+
+
+alert(resultadoDef);
+
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+// comentario en lina 
+
+/*comentario 
+en 
+bloque*/
+
+//prueba de funcionamiento de js
+
+//document.write("si funciona mi js");
+
+//generar alerta a usuario
+//alert("ventana de alerta para usuario");
+
+//solicitar informacion a n uestro uuario
+ /*var nombre=prompt("pro favor introduzca su nombre");
+ 
+//solicitar informacion a nuestro usuario
+var nombre = prompt("por favor introduzca su nombre");
+document.write("<h1>bienvenido: " + nombre + "</h1><br/>");
+
+var edad=promt("por favor introduzca su edad");
+document.write("su edad es: " + edad + " años");*/
+
+/*operaciones matematicas
+
+var numUno = 5;
+
+var numDos = 2;
+
+var munTres = 2
+
+var suma = numUno + numDos;
+var resultado = suma / numTres;
+console.log(suma);
+console.log(resultado)*/
+
+//operaciones matematicas pesonalizada
+
+/*var numUno = parseInt(prompt("introduzca primer valor"));
+
+var numDos =  parseInt(prompt("introduzca segundo valor"));
+
+var numTres =  parseInt(prompt("introduzca tercer valor"));
+
+var resultado = (numUno + numDos) / numTres;
+
+var suma = numUno + numDos;
+var resultado = suma / numTres;
+console.log("la operacion es la siguiente " + numUno + " + " + numDos + " / " + numTres);
+console.log(resultado)*/
+
+/*variables boolianas
+var numUno = parseInt(prompt("introduzca un numero"))
+var numDos = parseInt(prompt("introduzca un numero"))
+
+var resultado = numUno >= 18;
+console.log(resultado);*/
+
+//variable arrayconocidas como cadenas o arreglos
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto", "Septiembre", "Octubre", "Novimbre", "Diciembre"]
+
+console.log(meses);
+
+console.log(meses[3])
